c8afc51dc2950ecf030c69bdd657d504bc304f84
Randomaizer <altair@xaker.ru>
2022-02-10 16:25:18 +0300

    Update m3u.sh

--- m3u.sh
+++ m3u.sh
@@ -15,18 +15,21 @@ then
 echo "usage m3u.sh /opt/rclone/Film ftp://admin:za920600ur@svyaznoy362.synology.me TEST"
 exit 0
 fi
-#pattern=$(echo $3 | sed "s+$1+$2+g")
 pattern=$2
 echo pattern is $pattern
 echo "#EXTM3U" > /opt/$3.m3u
+
 find "$1" -type f -name *.mkv -o -name *.iso -o -name *.m2ts -o -name *.mp4 -o -name *.mp3 -o -name *.flac | sed "s+$1+$2+g" > /opt/$3.txt
+
 while IFS= read -r line; do
-#    echo $line | sed "s+$2+$1+g"
+#exclude xattr folder
+
     echo $line | grep ".xattr"
     if [ $? -eq 0 ]
     then
     continue
     fi
+    
     FILESIZE=$(ls -lh "$(echo $line | sed "s+$2+$1+g")" | awk '{print  $5}')
     echo "Size of $line = $FILESIZE"
     name=$(echo $line | sed "s+$pattern++g" | rev | cut -d "/" -f 2- | rev )
@@ -36,12 +39,11 @@ while IFS= read -r line; do
     poster_path=${post%.*}
     postter=$poster_path"-poster.jpg"
 
-#   get kp id
-    guess_kp=$(curl -k --silent -G "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword" --data-urlencode "keyword=$rev_name" -H "accept: application/json" -H "X-API-KEY: $KEY" | jq -r '.films[0].filmId')
+#   get kp id from name search
     real_kp=$(echo $line | grep -oP 'kp-\K\w+')
     if [ -z $real_kp ]
     then
-    kp=$guess_kp
+    kp=$(curl -k --silent -G "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword" --data-urlencode "keyword=$rev_name" -H "accept: application/json" -H "X-API-KEY: $KEY" | jq -r '.films[0].filmId')
     else
     kp=$real_kp
     fi
